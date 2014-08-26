6551f31d8449a372240cc1dfc0f189bdbf025590
Nicolas Miyasato <nmiyasato@gmail.com>
2014-08-26 17:10:21 -0300

    Adding LogViewController

---
+++ SampleSDKSwift/LogViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LogViewController.swift
+//  SampleSDKSwift
+//
+//  Created by Nicolas Miyasato on 8/26/14.
+//  Copyright (c) 2014 Kidozen. All rights reserved.
+//
+
+import Foundation
+
+class LogViewController : UIViewController {
+    
+}
