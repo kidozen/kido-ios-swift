8f977908e63d6a60aa21d476d715efa96ad829c0
Nicolas Miyasato <nmiyasato@gmail.com>
2014-08-26 12:07:31 -0300

    Adding services files

---
+++ SampleSDKSwift/ServicesViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ServicesViewController.swift
+//  SampleSDKSwift
+//
+//  Created by Nicolas Miyasato on 8/26/14.
+//  Copyright (c) 2014 Kidozen. All rights reserved.
+//
+
+import Foundation
