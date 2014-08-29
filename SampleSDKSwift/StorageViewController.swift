5aac3f90e1ae623e004344aa92e25b70c858b8f3
Nicolas Miyasato <nmiyasato@gmail.com>
2014-08-29 15:29:07 -0300

    Adding StorageViewController

---
+++ SampleSDKSwift/StorageViewController.swift
@@ -0,0 +1,13 @@
+//
+//  StorageViewController.swift
+//  SampleSDKSwift
+//
+//  Created by Nicolas Miyasato on 8/29/14.
+//  Copyright (c) 2014 Kidozen. All rights reserved.
+//
+
+import Foundation
+
+class StorageViewController : UIViewController {
+    
+}
