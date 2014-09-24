9a921d7b82c07fe553e57dbd1722a5080b245e1b
Nicolas Miyasato <nmiyasato@gmail.com>
2014-09-24 16:35:33 -0300

    Initial

--- KidoZenSwiftSDK/KidoZenSwiftSDK.h
+++ KidoZenSwiftSDK/KidoZenSwiftSDK.h
@@ -7,6 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for KidoZenSwiftSDK.
 FOUNDATION_EXPORT double KidoZenSwiftSDKVersionNumber;
@@ -14,6 +15,7 @@ FOUNDATION_EXPORT double KidoZenSwiftSDKVersionNumber;
 //! Project version string for KidoZenSwiftSDK.
 FOUNDATION_EXPORT const unsigned char KidoZenSwiftSDKVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <KidoZenSwiftSDK/PublicHeader.h>
-
+#import "AFNetworking.h"
+#import "CrashReporter.h"
+#import "SRWebSocket.h"
 
---
+++ KidoZenSwiftSDK/MyClass.swift
@@ -0,0 +1,18 @@
+//
+//  MyClass.swift
+//  KidoZenSwiftSDK
+//
+//  Created by Nicolas Miyasato on 9/24/14.
+//  Copyright (c) 2014 KidoZen. All rights reserved.
+//
+
+import Foundation
+
+public class MyClass {
+    
+//    var crash : CrashReporter?
+    
+    public init() {
+        
+    }
+}
