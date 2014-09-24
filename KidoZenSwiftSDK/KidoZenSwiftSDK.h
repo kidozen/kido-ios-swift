d4063446a192ecb488995df561645471e5b01a97
Nicolas Miyasato <nmiyasato@gmail.com>
2014-09-24 15:56:26 -0300

    Initial Commit

---
+++ KidoZenSwiftSDK/KidoZenSwiftSDK.h
@@ -0,0 +1,19 @@
+//
+//  KidoZenSwiftSDK.h
+//  KidoZenSwiftSDK
+//
+//  Created by Nicolas Miyasato on 9/24/14.
+//  Copyright (c) 2014 KidoZen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KidoZenSwiftSDK.
+FOUNDATION_EXPORT double KidoZenSwiftSDKVersionNumber;
+
+//! Project version string for KidoZenSwiftSDK.
+FOUNDATION_EXPORT const unsigned char KidoZenSwiftSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KidoZenSwiftSDK/PublicHeader.h>
+
+
