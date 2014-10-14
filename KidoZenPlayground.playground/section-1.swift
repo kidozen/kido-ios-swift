4b5a9c804c501a1265748f97333219c9c4dc3425
Nicolas Miyasato <nmiyasato@gmail.com>
2014-10-14 17:44:04 -0300

    Adding playground

---
+++ KidoZenPlayground.playground/section-1.swift
@@ -0,0 +1,15 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+import KidoZenSwiftSDK
+
+var str = "Hello, playground"
+
+var kzApplication = KZApplication(tenantMarketPlace: "", applicationName: "", applicationKey: "", strictSSL: false)
+
+var ds = kzApplication.datasource(name: "asdf")
+
+ds.invoke(data: nil, willStartCb: nil, success: nil, failure: nil)
+
+
+
