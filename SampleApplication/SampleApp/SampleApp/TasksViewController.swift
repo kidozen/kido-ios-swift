9727f394454eda3aa1477d60110116cf3105e039
Nicolas Miyasato <nmiyasato@gmail.com>
2014-12-03 16:48:47 -0300

    Adding tasks controller

---
+++ SampleApplication/SampleApp/SampleApp/TasksViewController.swift
@@ -0,0 +1,13 @@
+//
+//  TasksViewController.swift
+//  SampleApp
+//
+//  Created by Nicolas Miyasato on 12/3/14.
+//  Copyright (c) 2014 KidoZen. All rights reserved.
+//
+
+import UIKit
+
+class TasksViewController : UIViewController {
+    
+}
