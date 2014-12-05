ee29382e45b1f9a6a7e4eb1db32f1c4485a7c468
Nicolas Miyasato <nmiyasato@gmail.com>
2014-12-05 14:57:13 -0300

    Working on adding new task.

--- SampleApplication/SampleApp/SampleApp/AddTaskViewController.swift
+++ SampleApplication/SampleApp/SampleApp/AddTaskViewController.swift
@@ -10,8 +10,44 @@ import UIKit
 
 class AddTaskViewController : UIViewController  {
     
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var okButton: UIButton!
+    @IBOutlet weak var descriptionTextView: UITextView!
+    @IBOutlet weak var featureButton: UIButton!
+    
+    @IBOutlet weak var bugFeatureButton: UIButton!
+    
+    deinit {
+        NSNotificationCenter.defaultCenter().removeObserver(self)
+    }
+    
+    
     override func viewDidLoad() {
-        self.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Cancel, target: self, action: "cancel")
+        self.title = "New Task"
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Cancel,
+                                                                             target: self,
+                                                                             action: "cancel")
+        
+        
+        self.bugFeatureButton.backgroundColor = UIColor.lightGrayColor()
+        self.featureButton.backgroundColor = UIColor.whiteColor()
+        self.bugFeatureButton.selected = true
+        self.featureButton.selected = false
+
+        var sz = UIScreen.mainScreen().bounds.size
+        sz.height = sz.height - self.scrollView.frameY
+        self.scrollView.frameSize = sz
+        
+        self.scrollView.contentSize = CGSizeMake(sz.width, self.okButton.frameMaxY + 5)
+        
+        self.registerForKeyboardNotifications()
+        addHideTapGestureRecognizer()
+    }
+    
+    func registerForKeyboardNotifications() {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardDidShow:", name: UIKeyboardDidShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardWillHideNotification, object: nil)
     }
     
     func cancel() {
@@ -20,4 +56,65 @@ class AddTaskViewController : UIViewController  {
         })
     }
     
+    func addHideTapGestureRecognizer() {
+        let gr = UITapGestureRecognizer(target: self, action: "hideKeyboard")
+        self.view.addGestureRecognizer(gr)
+    }
+    
+    func hideKeyboard() {
+        if (self.titleTextField.isFirstResponder()) {
+            self.titleTextField.resignFirstResponder()
+        } else if (self.descriptionTextView.isFirstResponder()) {
+            self.descriptionTextView.resignFirstResponder()
+        }
+    }
+    
+    func keyboardDidShow(notification:NSNotification) {
+        let keyboardHeight = self.heightForKeyboard(notification)
+        
+        self.scrollView.frameHeight = UIScreen.mainScreen().bounds.size.height - keyboardHeight
+        
+        self.scrollView.contentSize = CGSizeMake(UIScreen.mainScreen().bounds.size.width, self.okButton.frameMaxY + 5)
+    }
+    
+    func keyboardWillHide(notification:NSNotification) {
+       
+            self.scrollView.frameHeight = UIScreen.mainScreen().bounds.size.height
+            self.scrollView.contentSize = CGSizeMake(UIScreen.mainScreen().bounds.size.width, self.okButton.frameMaxY + 5)
+            self.scrollView.scrollRectToVisible(self.okButton.frame, animated: true)
+    }
+
+    func heightForKeyboard(notification:NSNotification) -> CGFloat {
+        let info = notification.userInfo
+        let value = info![UIKeyboardFrameBeginUserInfoKey] as NSValue
+        let size = value.CGRectValue().size
+        let orientation = UIApplication.sharedApplication().statusBarOrientation
+        
+        if (UIInterfaceOrientationIsPortrait(orientation)) {
+            return size.height
+        } else {
+            return size.width
+        }
+    }
+    
+    @IBAction func bugPressed(sender:AnyObject) {
+        self.bugFeatureButton.selected = true
+        self.featureButton.selected = false
+    
+        self.bugFeatureButton.backgroundColor = UIColor.lightGrayColor()
+        self.featureButton.backgroundColor = UIColor.whiteColor()
+    }
+    
+    @IBAction func featurePressed(sender:AnyObject) {
+        self.bugFeatureButton.selected = false
+        self.featureButton.selected = true
+    
+        self.bugFeatureButton.backgroundColor = UIColor.whiteColor()
+        self.featureButton.backgroundColor = UIColor.lightGrayColor()
+    
+    }
+
+    @IBAction func okPressed(sender: AnyObject) {
+        self.hideKeyboard()
+    }
 }
